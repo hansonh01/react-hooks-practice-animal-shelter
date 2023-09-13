ec27baad16712d97910738b2173f1b53a5b2e874
Hanson <hanson.hua903@gmail.com>
2023-09-12 19:00:04 -0500

    Run2:Added extra show adopted button

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,28 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const handleSetFilter = (e) => {
+    setFilters({ type: e.target.value })
+  };
+  
+  const handleFilter = () => {
+    let url = 'http://localhost:3001/pets';
+
+    if(filters.type !== "all"){
+      url += `?type=${filters.type}`
+    };
+
+    fetch(url)
+      .then(r=>r.json())
+      .then(setPets)
+  };
+
+  const showAdopted = () => {
+    fetch('http://localhost:3001/pets?isAdopted=true')
+      .then(r=>r.json())
+      .then(setPets)
+  };
 
   return (
     <div className="ui container">
@@ -15,10 +37,14 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+            onSelectingFilter={handleSetFilter} 
+            onFilter={handleFilter} 
+            showAdopted={showAdopted}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} />
           </div>
         </div>
       </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Pet() {
+function Pet({pet}) {
+  const { id, type, gender, age, weight, name, isAdopted} = pet;
+  const [adopt, setAdopt] = useState(isAdopted);
+
+  const handleAdopting = () => {
+    fetch(`http://localhost:3001/pets/${id}`,{
+      method:'PATCH',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({isAdopted:!adopt})
+    })
+      .then(r=>r.json())
+      .then(()=>{
+        setAdopt(!adopt)
+      })
+  };
+
   return (
     <div className="card" data-testid="pet">
       <div className="content">
         <span className="header">
-          {/*'♀' OR '♂' */}
-          PET NAME
+          {gender === "male" ? '♂' : '♀'}
+          {name}
         </span>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">{type}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p>Age: {age}</p>
+          <p>Weight: {weight}</p>
         </div>
       </div>
-      <div className="extra content">
-        <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
+      <div className="extra content" onClick={handleAdopting}>
+        {adopt ? 
+        <button className="ui disabled button">Already adopted</button> : 
+        <button className="ui primary button">Adopt pet</button>}
+        
       </div>
     </div>
   );
